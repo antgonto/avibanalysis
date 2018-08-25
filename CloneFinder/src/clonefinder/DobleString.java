1e3e6350b5ad4aa81fd5895612b429c47c2c378b
Juan <juanfvillacis@gmail.com>
2018-08-25 14:48:24 -0600

    cambios menores 2

--- CloneFinder/src/clonefinder/DobleString.java
+++ CloneFinder/src/clonefinder/DobleString.java
@@ -21,23 +21,38 @@ public class DobleString {
         this.strArriba=str1;
         this.strAbajo=str2;
     }
-    
+    /**
+     * Obtiene el string que va en la parte superior en la visualizacion de alineamientos de smith-waterman
+     * @return el string
+     */
     public String getStringArriba(){
         return strArriba;
     }
-    
+    /**
+     * Obtiene el string que va en la parte inferior en la visualizacion de alineamientos de smith-waterman
+     * @return el string
+     */
     public String getStringAbajo(){
         return strAbajo;
     }
-    
+    /**
+     * agrega un caracter al string superior
+     * @param s lo que se quiere agregar al string
+     */
     public void appendStringArriba(String s){
         strArriba=s+strArriba;
     }
-    
+    /**
+     * agrega un caracter al string inferior
+     * @param s lo que se quiere agregar al string
+     */
     public void appendStringAbajo(String s){
         strAbajo=s+strAbajo;
     }
     
+    /**
+     * imprime de forma bonita la visualizacion de alineamientos locales de smith-waterman
+     */
     public void prettyPrint(){
         System.out.println(strArriba);
         String a="";
